c8f8c3de3d3a12b5ebfbe84915b3e380e37dd843
Mariam Nzeyimana <Mariam.Nze@gmail.com>
2025-08-27 14:44:43 +0200

    Initial commit

---
+++ src/main/java/com/gretacvld/climb_jam_api/ClimbJamApiApplication.java
@@ -0,0 +1,13 @@
+package com.gretacvld.climb_jam_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimbJamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimbJamApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gretacvld/climb_jam_api/ClimbJamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gretacvld.climb_jam_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbJamApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
