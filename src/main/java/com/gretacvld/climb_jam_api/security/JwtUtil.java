b61b3ac95603ad5138a0f2a1459668c14aaa6d0b
Mariam Nzeyimana <Mariam.Nze@gmail.com>
2025-09-03 16:32:51 +0200

    JAM-44 Setup JWT token and Exceptions

--- src/main/java/com/gretacvld/climb_jam_api/security/JwtUtil.java
+++ src/main/java/com/gretacvld/climb_jam_api/security/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.gretacvld.climb_jam_api.security;
 
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 
 @Service
 public class JwtUtil {
@@ -20,5 +22,39 @@ public class JwtUtil {
     @PostConstruct
     public void init() {
         this.key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String getEmailFromToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key).build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validateJwtToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException e) {
+            throw  new SecurityException("Invalid JWT signature: " + e.getMessage());
+        } catch (MalformedJwtException e) {
+            throw new MalformedJwtException("Invalid JWT token: " + e.getMessage());
+        } catch (ExpiredJwtException e) {
+            throw new ExpiredJwtException(null, null, "JWT token is expired: " + e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            throw new UnsupportedJwtException("JWT token is unsupported: " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("JWT claims string is empty: " + e.getMessage());
+        }
     }
 }
