b5ef1fdfeccca135ad5d18f3afb610c20cacd265
Mariam Nzeyimana <Mariam.Nze@gmail.com>
2025-09-03 14:51:39 +0200

    JAM-44 Started security config + Setup JWT secret key

--- src/main/java/com/gretacvld/climb_jam_api/repositories/UserRepository.java
+++ src/main/java/com/gretacvld/climb_jam_api/repositories/UserRepository.java
@@ -7,8 +7,8 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByEmail(String email);
-    Optional<User> findByUsername(String username);
-    boolean existsByEmail(String email);
-    boolean existsByUsername(String username);
+    public Optional<User> findByEmail(String email);
+    public boolean existsByEmail(String email);
+    public Optional<User> findByUsername(String username);
+    public boolean existsByUsername(String username);
 }
---
+++ src/main/java/com/gretacvld/climb_jam_api/security/JwtUtil.java
@@ -0,0 +1,24 @@
+package com.gretacvld.climb_jam_api.security;
+
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+
+@Service
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+    @Value("${jwt.expiration}")
+    private int jwtExpirationMs;
+    private SecretKey key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(jwtSecret.getBytes(StandardCharsets.UTF_8));
+    }
+}
