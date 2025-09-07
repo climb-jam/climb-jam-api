7703c721024b8021708ec782f9c7ecad78765b52
Mariam Nzeyimana <Mariam.Nze@gmail.com>
2025-09-07 14:52:48 +0200

    JAM-44 Added validation exception handler

--- src/main/java/com/gretacvld/climb_jam_api/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/gretacvld/climb_jam_api/exceptions/GlobalExceptionHandler.java
@@ -5,12 +5,39 @@ import io.jsonwebtoken.MalformedJwtException;
 import io.jsonwebtoken.UnsupportedJwtException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(MethodArgumentNotValidException e) {
+
+        List<Map<String, String>> errors = e.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(error -> {
+                    Map<String, String> fieldError = new HashMap<>();
+                    fieldError.put("field", error.getField());
+                    fieldError.put("message", error.getDefaultMessage());
+                    return fieldError;
+                })
+                .collect(Collectors.toList());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("message", "Échec de la validation");
+        response.put("errors", errors);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
 
     @ExceptionHandler(EmailAlreadyUsedException.class)
     public ResponseEntity<String> handleEmailAlreadyUsed(EmailAlreadyUsedException e) {
